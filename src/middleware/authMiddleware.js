7e04a7686d07aed8e02bd4d35141903e284a6dcf
borges-moonflag <borges@agmoonflag.com.br>
2025-10-01 14:46:10 -0300

    fixed deployment

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,7 +4,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "super_secret_key";
 module.exports = (req, res, next) => {
   // Pega o token do cookie
   const token = req.cookies?.token;
-
+ 
   if (!token) return res.status(401).json({ message: "Token não fornecido" });
 
   try {
